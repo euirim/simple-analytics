4bcc7938c4b693056d3e4c219be4bd1cb73721d9
euirim <euirim@me.com>
2016-12-25 02:50:04 -0500

    suffix adding utils function improved and GATable bulked up

--- simplea/gatables.py
+++ simplea/gatables.py
@@ -1,11 +1,18 @@
+from utils import add_suffix
+
 class GATable(object):
     
-    # start and end dates in the format YYYY-MM-DD
     def __init__(self, table_id, api_key, start_date, end_date, metrics):
         self.table_id = table_id
         self.api_key = api_key
+        # start and end dates in the format YYYY-MM-DD
         self.start_date = start_date
         self.end_date = end_date
         # comma separated list of analytics metrics (no ga: suffix required)
-        self.metrics = metrics 
+        self.metrics = add_suffix(metrics, "ga:")
+        
+        self.dimensions = None
+        self.filters = None
+        self.sampling_level = None
+        self.sort = None
         return
