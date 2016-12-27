a422fc35890cac2bda74973e8e45ab259f7da853
euirim <euirim@me.com>
2016-12-27 12:20:59 -0500

    fixed bug in add_suffix

--- simplea/utils.py
+++ simplea/utils.py
@@ -6,6 +6,9 @@ def add_suffix(txt, suffix):
     add_suffix("hello,john", "ga:") -> "ga:hello,ga:john"
     """
 
+    if txt is None:
+        return None
+
     elements = txt.split(",")
     elements = ["-" + suffix + e[1:] if e[0] == "-" 
             else suffix + e for e in elements]
